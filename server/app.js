31f94b6d8ba59ed788e32d904f6259c47e6a5ce5
dinobi <dino.kennetcorp@gmail.com>
2017-07-11 04:25:25 +0100

    setup express and setup server in the www folder

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
+// Set up the express app as constant
+const app = express();
+
+// Log all requests to the console
+app.use(logger('dev'));
+
+// Parse incoming requests bodies in a middleware (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Setup a default catch-all route that sends back a success message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+  message: "Welcome to Andela Bootcamp 24 - PostIt App.";
+}));
+
+// Setup a default catch-all route that sends back an error message in JSON format.
+app.get('*', (req, res) => res.status(404).send({
+  message: "The resource was not found";
+}));
+
+module.exports = app;
