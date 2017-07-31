14e9fb638bd16226c70ac9f32c49d020de3afc06
dinobi <dino.kennetcorp@gmail.com>
2017-07-31 05:15:07 +0100

    save template files to client src

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,18 @@
-import react from 'react';
-import reactDOM from 'react-dom';
+import React from 'react';
 
-require('./style.css');
-document.write(require('./content.js'));
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to PostIt</h2>
+        </div>
+        <p className="App-intro">
+          Simplified group messaging
+        </p>
+      </div>
+    );
+  }
+}
 
+export default App;
--- client/src/content.js
+++
@@ -1,1 +0,0 @@
-module.exports = 'It works from content.js and on a different port.';
