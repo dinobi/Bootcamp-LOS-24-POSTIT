beaaccdea9fb1ae1d660c269c5fe9afec1d0e61a
dinobi <dino.kennetcorp@gmail.com>
2017-07-31 10:06:49 +0100

    test out react props

--- client/src/app.js
+++ client/src/app.js
@@ -3,17 +3,9 @@ import Base from './css/base.css';
 
 class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Welcome to PostIt</h1>
-          <h3>Communicate Group Messages Instantly</h3>
-        </div>
-        <p className="App-intro">
-          PostIt is built to allow family, friends and colleagues meetup in groups to share
-           messages in a fun way whenever they want.
-        </p>
-      </div>
+    let myText = this.props.myText;
+    return (      
+      <h1>{myText}</h1>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App myText = 'This is my first Prop'/>, document.getElementById('root'));
