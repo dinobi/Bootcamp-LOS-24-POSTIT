ede0544481090445d82b41bd1a921d8dd0c55c65
dinobi <dino.kennetcorp@gmail.com>
2017-10-19 10:18:30 +0100

    chore(refactor): relocate directories appropriately

--- server/bin/www/index.js
+++ server/bin/www/index.js
@@ -1,6 +1,7 @@
 // This will be our application entry. We'll setup our server here.
+require('dotenv').config();
 const http = require('http');
-const app = require('../../../app'); // The created express app
+const app = require('../../app'); // The created express app
 
 const port = parseInt(process.env.PORT, 10) || 3001;
 app.set('port', port);
