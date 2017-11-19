9e8045e4a5647651fe4ececba53d3a41110137c5
dinobi <dino.kennetcorp@gmail.com>
2017-11-19 18:31:40 +0100

    fix(model-test): fix model failing test on travis

--- test/userModel.spec.js
+++ test/userModel.spec.js
@@ -28,7 +28,7 @@ describe('User Model', () => {
     };
     models.User.create(newUser)
     .catch((error) => {
-      expect(error.errors[0].message).to.equal('username cannot be null');
+      expect(error.errors[0].message).to.equal('User.username cannot be null');
       done();
     });
   });
@@ -41,7 +41,7 @@ describe('User Model', () => {
     };
     models.User.create(newUser)
     .catch((error) => {
-      expect(error.errors[0].message).to.equal('email cannot be null');
+      expect(error.errors[0].message).to.equal('User.email cannot be null');
       done();
     });
   });
@@ -54,7 +54,7 @@ describe('User Model', () => {
     };
     models.User.create(newUser)
     .catch((error) => {
-      expect(error.errors[0].message).to.equal('phone cannot be null');
+      expect(error.errors[0].message).to.equal('Usesr.phone cannot be null');
       done();
     });
   });
@@ -67,7 +67,7 @@ describe('User Model', () => {
     };
     models.User.create(newUser)
     .catch((error) => {
-      expect(error.errors[0].message).to.equal('password cannot be null');
+      expect(error.errors[0].message).to.equal('User.password cannot be null');
       done();
     });
   });
