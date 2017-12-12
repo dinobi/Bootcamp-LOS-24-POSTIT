c3ddf7dbc45b9412e1581850429e867a7b2638a9
dinobi <dino.kennetcorp@gmail.com>
2017-12-12 09:58:09 +0100

    fix(missing-inputValidator): add missing inputValidator.js file

---
+++ server/helpers/inputValidator.js
@@ -0,0 +1,150 @@
+/* eslint-disable no-useless-escape */
+import errorResponse from './errorResponse';
+
+const emailRE = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+const alphanumeric = /^[a-zA-Z0-9_]*$/;
+
+/**
+ * signupValidator
+ * validates users input during signup process
+ *
+ * @returns {object} res - response object when check fails
+ * @returns {string} validated - value when check passes
+ * @param {object} req - response object
+ * @param {object} res - response object
+ */
+const signupValidator = (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const phone = req.body.phone;
+  let message;
+
+  if (!username || username.trim() === '') {
+    message = 'username field cannot be empty';
+    return errorResponse(res, 400, message, null);
+  }
+  if (username.length < 3) {
+    message = 'username should be atleast 3 characters long';
+    return errorResponse(res, 400, message, null);
+  }
+  if (username.length > 18) {
+    message = 'username should not exceed 18 characters';
+    return errorResponse(res, 413, message, null);
+  }
+  if (!alphanumeric.test(username)) {
+    message = 'username can contain only alphabets, numbers, and underscore';
+    return errorResponse(res, 400, message, null);
+  }
+  if (!email || email.trim() === '') {
+    message = 'email field cannot be empty';
+    return errorResponse(res, 400, message, null);
+  }
+  if (!emailRE.test(email)) {
+    message = 'Enter a valid email';
+    return errorResponse(res, 400, message, null);
+  }
+  if (!password || password.trim() === '') {
+    message = 'password field cannot be empty';
+    return errorResponse(res, 400, message, null);
+  }
+  if (password.length < 6) {
+    message = 'password should be up to 6 characters long';
+    return errorResponse(res, 400, message, null);
+  }
+  if (!phone || phone.trim() === '') {
+    message = 'phone field cannot be empty';
+    return errorResponse(res, 400, message, null);
+  }
+  return 'validated';
+};
+
+/**
+ * loginValidator
+ * validates users input during signin process
+ *
+ * @returns {object} res - response object when check fails
+ * @returns {string} validated - value when check passes
+ * @param {object} req - response object
+ * @param {object} res - response object
+ */
+const loginValidator = (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  let message;
+  if (!username || username.trim() === '') {
+    message = 'username field cannot be empty';
+    return errorResponse(res, 400, message, null);
+  }
+
+  if (!password || password.trim() === '') {
+    message = 'password field cannot be empty';
+    return errorResponse(res, 400, message, null);
+  }
+
+  return 'validated';
+};
+
+/**
+ * loginValidator
+ * validates users input for uniqueness and validity
+ * during signup process
+ *
+ * @returns {object} res - response object when check fails
+ * @returns {string} validated - value when check passes
+ * @param {object} res - response object
+ * @param {object} error - error response object
+ */
+const uniqueValidator = (res, error) => {
+  const uniqueError = error.errors[0].message;
+  let message;
+  switch (uniqueError) {
+    case 'username must be unique':
+      message = 'Username already exists';
+      return errorResponse(res, 409, message, null);
+    case 'email must be unique':
+      message = 'Email already exists';
+      return errorResponse(res, 409, message, null);
+    case 'Validation isEmail on email failed':
+      message = 'Enter a valid email';
+      return errorResponse(res, 400, message, null);
+    case 'Validation not on phone failed':
+      message = 'Enter a valid phone';
+      return errorResponse(res, 400, message, null);
+    default:
+      return 'validated';
+  }
+};
+
+/**
+ * messageValidator
+ * validates user created messages
+ *
+ * @returns {object} res - response object when check fails
+ * @returns {string} validated - value when check passes
+ * @param {string} message - user created message
+ * @param {object} priority - user selected priority
+ * @param {object} req - response object
+ * @param {object} res - response object
+ */
+const messageValidator = (message, priority, req, res) => {
+  let errorMessage;
+  if (!message || message.trim() === '') {
+    errorMessage = 'You forgot to include a message body';
+    return errorResponse(res, 400, errorMessage, null);
+  }
+  if (!priority || priority === '') {
+    errorMessage = 'Priority level is required';
+    return errorResponse(res, 400, errorMessage, null);
+  }
+  if (priority !== undefined &&
+    priority.toLowerCase() !== 'urgent'
+    && priority.toLowerCase() !== 'critical'
+    && priority.toLowerCase() !== 'normal') {
+    errorMessage = 'Invalid priority level';
+    return errorResponse(res, 400, errorMessage, null);
+  }
+  return 'validated';
+};
+
+export { signupValidator, loginValidator, uniqueValidator, messageValidator };
